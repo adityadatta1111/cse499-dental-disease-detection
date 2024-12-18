04760ea32a60a62ae8d63f97e03aa32341de8d27
adityadatta1111 <adityadatta111@gmail.com>
2024-12-19 05:13:42 +0600

    Finished the implementations so far, of the interactive GUI, with adjustable bounding box saving feature

--- app.py
+++ app.py
@@ -16,6 +16,16 @@ st.set_page_config(
 
 st.title("Dental Image Detection and Prediction Using YOLOv8 and SAM")
 
+# Initialize session states
+if 'img_file' not in st.session_state:
+    st.session_state.img_file = None
+
+if 'cropped_img' not in st.session_state:
+    st.session_state.cropped_img = None
+
+if 'label' not in st.session_state:
+    st.session_state.label = ""
+
 st.sidebar.header("DL Model Configuration")
 
 model_type = st.sidebar.radio(
@@ -48,6 +58,7 @@ source_radio = st.sidebar.radio(
 )
 
 source_img = None
+img_file = None
 
 if source_radio == settings.IMAGE:
     source_img = st.sidebar.file_uploader(
@@ -69,14 +80,15 @@ if source_radio == settings.IMAGE:
                 progress_bar = st.progress(0)
 
                 if uploaded_image is not None:
-
                     # Upload progress
                     for percent_complete in range(0, 101, 10):
                         time.sleep(0.1)  # Simulate upload delay
                         progress_bar.progress(percent_complete)
                     progress_bar.empty()
+
+                st.session_state.img_file = uploaded_image  # Save the uploaded image to session state
                 st.image(source_img, caption="Uploaded Image Successfully")
-                
+
         except Exception as ex:
             st.error(f"Error loading image: {ex}")
 
@@ -95,18 +107,13 @@ if source_radio == settings.IMAGE:
                 res_plotted = res[0].plot()[:, :, ::-1]
                 st.image(res_plotted, caption='Detected Image',
                          use_column_width=True)
-
-                try:
-                    with st.expander("Detectioin Results"):
-                        for box in boxes:
-                            st.write(box.data)
-                            class_label = model.names[int(box.cls)]
-                            st.write(class_label)
-                except Exception as ex:
-                    st.write("No image has been uploaded.")
+                st.session_state.img_file = Image.fromarray(res_plotted)  # Update session state with detected image
+                st.session_state.img_file.save("detected_image.jpg")
+                Image.open("detected_image.jpg")
 
     with st.container():
-        # Check`custom-labels` folder exists
+        
+        # Check `custom-labels` folder exists
         os.makedirs("custom-labels", exist_ok=True)
 
         # Save bounding coordinates
@@ -120,8 +127,7 @@ if source_radio == settings.IMAGE:
             # Save label to custom-labels folder
             label_path = os.path.join("custom-labels", f"{filename}.txt")
             with open(label_path, "w") as f:
-                f.write(f"{label} {x_center:.6f} {y_center:.6f} {
-                        width:.6f} {height:.6f}\n")
+                f.write(f"{label} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
 
             # Save cropped image to custom-labels folder
             image_path = os.path.join("custom-labels", f"{filename}.jpg")
@@ -135,9 +141,10 @@ if source_radio == settings.IMAGE:
             path = os.path.join("custom-labels", f"{filename}.jpg")
             image.save(path)
             st.success(f"Cropped image saved as {path}")
+        
         st.divider()
         # Streamlit UI setup
-        st.title("Annonation Tool")
+        st.title("Annotation Tool")
 
         # Sidebar controls
         realtime_update = st.checkbox(
@@ -149,61 +156,42 @@ if source_radio == settings.IMAGE:
             16, 9), "4:3": (4, 3), "2:3": (2, 3), "Free": None}
         aspect_ratio = aspect_dict[aspect_choice]
 
-        # Upload image
-        img_file = st.file_uploader(
-            "Upload an Image", type=['png', 'jpg', 'jpeg'])
-        
-        # Placeholder for the progress bar
-        progress_bar = st.progress(0)
-
-        if img_file is not None:
-
-            # Upload progress
-            for percent_complete in range(0, 101, 10):
-                time.sleep(0.1)  # Simulate upload delay
-                progress_bar.progress(percent_complete)
-            st.image(img_file, width=500, caption="Uploaded Image Successfully")
-            progress_bar.empty()
-
-        if img_file:
+        if st.session_state.img_file:
             try:
-                # Load and display the uploaded image
-                img = Image.open(img_file)
-                img_width, img_height = img.size
+                img = "/Users/adityadatta/Documents/programming/cse499-dental-disease-detection/detected_image.jpg"
+                img_file = Image.open(img)
+                img_width, img_height = img_file.size
 
-                st.write("Original Image:")
-                st.image(img, width=500)
+                st.write("Predicted Image:")
+                st.image(img_file, width=300)
 
                 # Cropping using Streamlit cropper tool
-                cropped_img = st_cropper(
-                    img, realtime_update=realtime_update, box_color=box_color, aspect_ratio=aspect_ratio)
+                st.session_state.cropped_img = st_cropper(
+                    img_file, realtime_update=realtime_update, box_color=box_color, aspect_ratio=aspect_ratio)
                 st.write("Cropped Image Preview:")
-                st.image(cropped_img, width=500)
+                st.image(st.session_state.cropped_img, width=500)
 
                 # Input for label
-                label = st.text_input(
-                    "Enter Label for the bounding box and then hit enter:", "")
-
-                # Label entered by the user
-                # Display the raw value entered
-                st.write(f"Entered Label: '{label}'")
+                st.session_state.label = st.text_input("Enter Label for the bounding box and then hit enter:", st.session_state.label)
+                st.write(f"Entered Label: '{st.session_state.label}'")
 
                 # Check if label is empty
-                if not label:
+                if not st.session_state.label:
                     st.error("Label cannot be empty.")
                 else:
                     # Bounding box coordinates
-                    left, upper, right, lower = cropped_img.getbbox()
+                    left, upper, right, lower = st.session_state.cropped_img.getbbox()
                     bbox = (left, upper, right, lower)
 
                     # File saving buttons
-                    filename = os.path.splitext(img_file.name)[0]
+                    # Use `source_img.name` to get the file name for saving
+                    filename = os.path.splitext(source_img.name)[0]
                     if st.button("Save Cropped Image"):
-                        save_cropped_image(cropped_img, filename)
+                        save_cropped_image(st.session_state.cropped_img, filename)
 
                     if st.button("Save YOLO Label"):
                         save_img_label_yolo_format(
-                            filename, img_width, img_height, bbox, label, cropped_img)
+                            filename, img_width, img_height, bbox, st.session_state.label, st.session_state.cropped_img)
 
             except Exception as e:
                 st.error(f"An error occurred: {e}")
